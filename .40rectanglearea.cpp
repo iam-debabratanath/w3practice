8642fea916f5c694234663d7d5d8597f0c73bf3d
iam-debabratanath <debabratanath1000@gmail.com>
2024-12-26 17:02:00 +0600

    Create .40rectanglearea.cpp

---
+++ .40rectanglearea.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    float width, height;
+    cin>>width>>height;
+    cout<< "The area is: "<<width*height<<endl;
+    cout<<"The perimeter is: "<<2*(width + height)<<endl;
+    return 0;
+}
