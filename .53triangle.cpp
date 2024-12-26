2da4e73ff7b9cabf1de89495e9faddd7ff4e31f4
iam-debabratanath <debabratanath1000@gmail.com>
2024-12-26 19:49:05 +0600

    Create .53triangle.cpp

---
+++ .53triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#include<math.h>
+
+int main() 
+{
+    float s1;
+    float area;
+    cin >> s1;
+    area = sqrt(3) / 4 * (s1 * s1);
+    cout << " The area of the equilateral triangle is: " << area << endl;
+    cout << endl;
+    return 0;
+}
