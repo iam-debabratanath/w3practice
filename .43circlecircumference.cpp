efb4a2317409535fc3193e408f1333b7c4c53bad
iam-debabratanath <debabratanath1000@gmail.com>
2024-12-26 18:50:16 +0600

    Create .43circlecircumference.cpp

---
+++ .43circlecircumference.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define PI 3.14159
+using namespace std;
+
+int main() 
+{
+    float radius;
+    cin>>radius;
+    cout<<PI*radius*radius<< " " <<2*PI*radius<<endl;
+    return 0;
+}
